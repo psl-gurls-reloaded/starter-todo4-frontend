753b21b5688e546e4963c4540724c9835c7c3950
Hiram Cabrera <takitocabrera@gmail.com>
2017-12-03 11:47:59 -0800

    Completed FrontEnd Part of lab

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -39,7 +39,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |  $autoload['packages'] = array(APPPATH.'third_party', '/usr/local/shared');
 |
 */
-$autoload['packages'] = array();
+//$autoload['packages'] = array();
+$autoload['packages'] = array(APPPATH.'third_party/restful/');
 
 /*
 | -------------------------------------------------------------------
@@ -58,7 +59,8 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('parser','parsedown','session');
+$autoload['libraries'] = array('parser','parsedown','session', 'curl', 'format', 'rest');
+
 
 /*
 | -------------------------------------------------------------------
--- application/core/XML_Model.php
+++ application/core/XML_Model.php
@@ -46,36 +46,47 @@ class XML_Model extends Memory_Model
 	 */
 	protected function load()
 	{
-	    //Checking if the file exists
-        if (file_exists($this->_origin)) {
-            $xml = simplexml_load_file($this->_origin);
-        } else {
-            exit('Failed to open data.xml.');
-        }
+//	    //Checking if the file exists
+//        if (file_exists($this->_origin)) {
+//            $xml = simplexml_load_file($this->_origin);
+//        } else {
+//            exit('Failed to open data.xml.');
+//        }
+//
+//        //Update the DB so called Titles (id, flag, etc) into an
+//        //array from memory_model named _fields
+//        $something = $xml->item;
+//        $fields = array();
+//       foreach ($something->children() as $key){
+//           array_push($fields,$key->getName());
+//       }
+//        $this->_fields = $fields;
+//
+//        // build the list of tasks
+//        foreach ($xml->item as $item) {
+//            // Creates rows of data stored in data in format:
+//            // ( [1] => stdClass Object ( [id] => 1 [task] => COMP1234 Assignment [priority] => 2 [size] => 2 [group] => 2 [deadline] => 20170219 [status] => 2 [flag] => )
+//            $record = new stdClass();
+//            for ($i = 0; $i < count($this->_fields); $i ++ )
+//                $record->{$this->_fields[$i]} = (string)$item->{$this->_fields[$i]};
+//            $key = $record->{$this->_keyfield};
+//            $this->_data[$key] = $record;
+//        }
+//
+//        $this->reindex();
+        // load our data from the REST backend
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $this->_data =  $this->rest->get('job');
 
-        //Update the DB so called Titles (id, flag, etc) into an
-        //array from memory_model named _fields
-        $something = $xml->item;
-        $fields = array();
-       foreach ($something->children() as $key){
-           array_push($fields,$key->getName());
-       }
-        $this->_fields = $fields;
-
-        // build the list of tasks
-        foreach ($xml->item as $item) {
-            // Creates rows of data stored in data in format:
-            // ( [1] => stdClass Object ( [id] => 1 [task] => COMP1234 Assignment [priority] => 2 [size] => 2 [group] => 2 [deadline] => 20170219 [status] => 2 [flag] => )
-            $record = new stdClass();
-            for ($i = 0; $i < count($this->_fields); $i ++ )
-                $record->{$this->_fields[$i]} = (string)$item->{$this->_fields[$i]};
-            $key = $record->{$this->_keyfield};
-            $this->_data[$key] = $record;
-        }
+        // rebuild the field names from the first object
+        $one = array_values((array) $this->_data);
+        $this->_fields = array_keys((array)$one[0]);
 
         $this->reindex();
+
     }
-        
+
 
 	/**
 	 * Store the collection state appropriately, depending on persistence choice.
@@ -83,36 +94,36 @@ class XML_Model extends Memory_Model
 	 */
 	protected function store()
 	{
-		// rebuild the keys table
-		$this->reindex();
-		//---------------------
-        //Create empty XML document
-        $doc = new DOMDocument();
-        if ($doc) {
-            // we want a nice output
-            $doc->formatOutput = true;
-
-            $top = $doc->createElement('tasks');
-            $top = $doc->appendChild($top);
-
-            //Iterate through each record in data (each record has an item object
-            foreach ($this->_data as $key => $record) {
-
-                $root = $doc->createElement('item');
-                $root = $top->appendChild($root);
-
-                //Iterate through each predefined _field and create xml tag <fieldname> field content </fieldname>
-                for($i = 0; $i < count($this->_fields); $i ++ ){
-                    $element = $doc->createElement($this->_fields[$i], $record->{$this->_fields[$i]});
-                    $element = $root->appendChild($element);
-                }
-            }
-
-            //Saves $doc content into the origin (aka, local address of file)
-            $doc->save($this->_origin);
-        } else {
-            exit('Failed to store data in tasks.xml.');
-        }
+//		// rebuild the keys table
+//		$this->reindex();
+//		//---------------------
+//        //Create empty XML document
+//        $doc = new DOMDocument();
+//        if ($doc) {
+//            // we want a nice output
+//            $doc->formatOutput = true;
+//
+//            $top = $doc->createElement('tasks');
+//            $top = $doc->appendChild($top);
+//
+//            //Iterate through each record in data (each record has an item object
+//            foreach ($this->_data as $key => $record) {
+//
+//                $root = $doc->createElement('item');
+//                $root = $top->appendChild($root);
+//
+//                //Iterate through each predefined _field and create xml tag <fieldname> field content </fieldname>
+//                for($i = 0; $i < count($this->_fields); $i ++ ){
+//                    $element = $doc->createElement($this->_fields[$i], $record->{$this->_fields[$i]});
+//                    $element = $root->appendChild($element);
+//                }
+//            }
+//
+//            //Saves $doc content into the origin (aka, local address of file)
+//            $doc->save($this->_origin);
+//        } else {
+//            exit('Failed to store data in tasks.xml.');
+//        }
 	}
 
 }
